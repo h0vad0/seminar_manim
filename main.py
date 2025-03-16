dceaeef41a58bac590fa072d762ee264f1983b6f
Tomas Duris <duristom@seznam.cz>
2025-03-16 20:46:42 +0100

    merge sort init

--- main.py
+++ main.py
@@ -57,12 +57,13 @@ class Sort(Scene):
         self.add(group)
         self.wait(1)
 
-        swaps = sort(array)
+        swaps = self.sort(array)
 
         for swap in swaps:
             self.play(Swap(group[swap[0]], group[swap[1]]))
             group.submobjects[swap[0]], group.submobjects[swap[1]] = group.submobjects[swap[1]], group.submobjects[swap[0]]
             self.wait(0.5)
+
 
 class BinarySearch(Scene):
     def construct(self):
@@ -148,4 +149,53 @@ class BinarySearch(Scene):
         self.play(group.animate.center())
         self.wait()
 
+                
+class MergeSort(Scene):
+    def construct(self):
+        arr = [5, 3, 8, 4, 2, 7, 1, 6]
+        group = Group()
+
+        for index, item in enumerate(arr):
+            square = Square(side_length=1.5)
+            number = Text(text=str(item))
+
+            square.add(number)
+            if index != 0:
+                square.next_to(group.submobjects[index-1], buff=0)
+
+            group.add(square)
+
+        scene_width = 14 + 2/9
+        spacing = 1
+
+        group.scale_to_fit_width(scene_width*0.6)
+        group.center()
+        self.add(group)
+        self.wait()
+
+        subgroups = [group]
+        while len(subgroups[-1]) != 1:
+            halves = []
+
+            for subgroup in subgroups:
+                mid = len(subgroup) // 2
+                half_l = subgroup[:mid]
+                halves.append(half_l)
+                half_r = subgroup[mid:]
+                halves.append(half_r)
+                self.play(half_l.animate.shift(LEFT*spacing), half_r.animate.shift(RIGHT*spacing))
+
+            subgroups = halves
+
+            spacing = spacing/2
+
+        sorted_list = []
+        i = j = 0
+
+        if left[i] < right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
 
